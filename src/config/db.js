c45b5eb9d6920ed79648a97516ab5beec584c41c
zinh8803 <ngoquocvinh2003@gmail.com>
2025-06-12 00:37:31 +0700

    fix cert

--- src/config/db.js
+++ src/config/db.js
@@ -4,17 +4,17 @@ const path = require('path');
 require("dotenv").config();
 
 const mysql2 = require('mysql2/promise');
-const caPath = path.resolve(__dirname, '../../certs/ca.pem');
+// const caPath = path.resolve(__dirname, '../../certs/ca.pem');
 const db = mysql2.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     port: process.env.DB_PORT,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    ssl: {
-        ca: fs.readFileSync(caPath),
-        rejectUnauthorized: true
-    },
+    // ssl: {
+    //     ca: fs.readFileSync(caPath),
+    //     rejectUnauthorized: true
+    // },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
