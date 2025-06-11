36324550560e7a04a89a9f4cdf02e10bc3a7c499
zinh8803 <ngoquocvinh2003@gmail.com>
2025-06-12 00:24:20 +0700

    add cert

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,20 @@
 const fs = require('fs');
-require("dotenv").config();
-const mysql2 = require('mysql2/promise');
+const path = require('path');
 
+require("dotenv").config();
+
+const mysql2 = require('mysql2/promise');
+const caPath = path.resolve(__dirname, '../../certs/ca.pem');
 const db = mysql2.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     port: process.env.DB_PORT,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    SSL: process.env.DB_SSL,
+    ssl: {
+        ca: fs.readFileSync(caPath),
+        rejectUnauthorized: true
+    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
