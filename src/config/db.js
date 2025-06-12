08bd7631fc56ec75304671639ee4f75eb2c60db8
Ngo Quoc Vinh <vinhnq@hybrid-technologies.vn>
2025-06-12 16:58:13 +0700

    fix v3.1

--- src/config/db.js
+++ src/config/db.js
@@ -4,24 +4,28 @@ const path = require('path');
 require("dotenv").config();
 
 const mysql2 = require('mysql2/promise');
-const DB_SSL = process.env.DB_SSL === 'true';
 const caPath = path.resolve(__dirname, '../../certs/ca.pem');
-const db = mysql2.createPool({
+
+const DB_SSL = process.env.DB_SSL === 'true';
+
+const config = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     port: process.env.DB_PORT,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
+};
 
-});
 if (DB_SSL) {
-    db.ssl = {
-        ca: fs.readFileSync(caPath), // dùng CA hợp lệ
+    config.ssl = {
+        ca: fs.readFileSync(caPath),
         rejectUnauthorized: true,
     };
 }
+
+const db = mysql2.createPool(config);
+
 module.exports = db;
